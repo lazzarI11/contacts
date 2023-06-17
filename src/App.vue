6f53ac417512b1d5bbfa3a271419e58d889bac04
asynch <=>
2023-06-17 19:20:58 +0200

    Added NavBar component and Added Basic Card to Home page

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
+  <NavBar/>
   <router-view/>
 </template>
 
 <script>
+import NavBar from "./components/NavBar"
   export default{
+    components: {NavBar
+  }
   }
 </script>
 
--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -1,5 +1,62 @@
 <template>
-    <h2>Contact Manager</h2>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <p class="h3 text-success fw bold">Contact Manager
+                <router-link to="/contacts/add" class="btn btn-success btn-sm">
+                    <i class="fa fa-plus-circle"></i>New</router-link>
+                </p>
+                <p class="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, eveniet porro quod odit rerum exercitationem mollitia accusamus delectus labore vero temporibus pariatur quis fugit molestiae explicabo veniam cum amet fugiat.</p>
+                <form>
+                    <div class="row">
+                       <div class="col-md-6">
+                            <div class="row">
+                                <div class="col">
+                                    <input type="text" class="form-control" placeholder="Search">
+                                </div>
+                                <div class="col">
+                                    <input type="submit" class="btn btn-outline-dark">
+                                </div>
+                            </div>
+                       </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="card my-2 list-group-item-success shadow-lg">
+                    <div class="card-body">
+                        <div class="row align-items-center">
+                            <div class="col-sm-4">
+                                <img src="https://img.icons8.com/?size=512&id=108652&format=png" alt="" class="contact-img">
+                            </div>
+                            <div class="col-sm-7">
+                                <ul class="list-group">
+                                    <li class="list-group-item">Name: <span class="fw-bold"> Name</span></li>
+                                    <li class="list-group-item">Email: <span class="fw-bold"> Email</span></li>
+                                    <li class="list-group-item">Mobile: <span class="fw-bold"> Mobile</span></li>
+                                </ul>
+                            </div>
+                            <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
+                                <router-link to="/contacts/view/:contactId" class="btn btn-warning my-1">
+                                    <i class="fa fa-eye"></i>
+                                </router-link>
+                                <router-link to="/contacts/edit/:contactId" class="btn btn-primary my-1">
+                                    <i class="fa fa-pen"></i>
+                                </router-link>
+                                <button class="btn btn-danger my-1">
+                                    <i class="fa fa-trash"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
