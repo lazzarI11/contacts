a70a645c6f0e60d8f166606657c4f1f25b160353
asynch <=>
2023-06-18 16:56:14 +0200

    added Spinner component,and finished ContantManager view

--- src/views/ContactManager.vue
+++ src/views/ContactManager.vue
@@ -24,20 +24,40 @@
             </div>
         </div>
     </div>
-    <div class="container mt-3">
+    <!--Spinner-->
+    <div v-if="loading">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <SpinnerLoader/>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--Error-->
+    <div v-if="!loading && errorMessage">
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col">
+                    <p class="h3 text-danger fw-bold">{{ errorMessage }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mt-3" v-if="contacts.length > 0">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6" v-for="contact of contacts" :key=  "contact">
                 <div class="card my-2 list-group-item-success shadow-lg">
                     <div class="card-body">
                         <div class="row align-items-center">
                             <div class="col-sm-4">
-                                <img src="https://img.icons8.com/?size=512&id=108652&format=png" alt="" class="contact-img">
+                                <img :src="contact.photo" alt="" class="contact-img">
                             </div>
                             <div class="col-sm-7">
                                 <ul class="list-group">
-                                    <li class="list-group-item">Name: <span class="fw-bold"> Name</span></li>
-                                    <li class="list-group-item">Email: <span class="fw-bold"> Email</span></li>
-                                    <li class="list-group-item">Mobile: <span class="fw-bold"> Mobile</span></li>
+                                    <li class="list-group-item">Name : <span class="fw-bold"> {{contact.name }}</span></li>
+                                    <li class="list-group-item">Email : <span class="fw-bold"> {{contact.email }}</span></li>
+                                    <li class="list-group-item">Mobile : <span class="fw-bold"> {{contact.mobile}}</span></li>
                                 </ul>
                             </div>
                             <div class="col-sm-1 d-flex flex-column justify-content-center align-items-center">
@@ -60,8 +80,35 @@
 </template>
 
 <script>
+import SpinnerLoader from "../components/SpinnerLoader"
+ import {contactService} from "@/services/contactService"
     export default{
-        name: "ContactManager"
+        components: {
+            SpinnerLoader
+        },
+        name: "ContactManager",
+        data: function(){
+            return{
+                loading: false,
+                contacts: [],
+                errorMessage : null
+            }
+        },
+        created : async function(){
+            try{
+                this.loading = true;
+                let response = await contactService.getAllContacts();
+                this.contacts = response.data.data;
+                this.loading = false;
+            }
+            catch(error){
+                this.errorMessage = error;
+                this.loading = false;
+            }
+        },
+        methods: {
+         
+        }
     }
 </script>
 
--- src/views/ViewContact.vue
+++ src/views/ViewContact.vue
@@ -1,5 +1,34 @@
 <template>
-    <h2>View Contact</h2>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col">
+                <p class="h3 text-success fw-bold">View Contact</p>
+                <p class="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus quibusdam eum ullam provident assumenda distinctio facere mollitia expedita, officiis esse corrupti ad magni doloremque! Consequuntur, tempore hic. Explicabo, fugit commodi.</p>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-4"> 
+                <img src="https://img.icons8.com/?size=512&id=108652&format=png" alt="" class="contact-img-big">
+            </div>
+            <div class="col-md-6">
+                <ul class="list-group">
+                    <li class="list-group-item">Name: <span class="fw-bold"> Name</span></li>
+                    <li class="list-group-item">Email: <span class="fw-bold"> Email</span></li>
+                    <li class="list-group-item">Mobile: <span class="fw-bold"> Mobile</span></li>
+                    <li class="list-group-item">Company: <span class="fw-bold"> Company</span></li>
+                    <li class="list-group-item">Title: <span class="fw-bold"> Title</span></li>
+                    <li class="list-group-item">Group: <span class="fw-bold"> Group</span></li>
+                </ul>
+            </div>
+        </div>
+        <div class="row mt-3">
+            <div class="column">
+                <router-link to="/" class="btn btn-success"><i class="fa fa-arrow-alt-circle-left"></i>Return</router-link>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
